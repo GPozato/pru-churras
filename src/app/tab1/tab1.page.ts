1b7e0e626b6150266cd7f4d1adaab6c460d5d085
Gabriel Pozato <gabriel.pozato@etec.sp.gov.br>
2023-02-13 21:56:47 -0300

    Finalizado Cara ou Coroa

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,10 +1,19 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import { runInThisContext } from 'vm';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  animations: [
+    trigger('rotatedState', [
+      state('default', style({transform: 'rotateY(0)'})),
+      state('rotaded', style({transform: 'rotateY(-360deg)'})),
+      transition('rotaded => default', animate('1500ms ease-out')),
+      transition('default => rotaded', animate('1500ms ease-in'))
+    ])
+  ]
 })
 export class Tab1Page {
 
@@ -12,16 +21,23 @@ export class Tab1Page {
   cara = 'assets/cara.png';
   coroa = 'assets/coroa.png';
   info = 'Clique em jogar a moeda!';
+  state = 'default';
 
   constructor() {}
 
   jogarMoeda(){
-    if (Math.random() < 0.5){
-      this.moeda = this.cara;
-      this.info = 'Cara!';
-    }else{
-      this.moeda = this.coroa;
-      this.info = 'Coroa!'
-    }
+    this.moeda = 'assets/logo.png';
+    this.info = 'Girando...';
+    this.state = (this.state === 'default') ? 'rotaded' : 'default';
+
+    setTimeout(() => {
+      if (Math.random() < 0.5){
+        this.moeda = this.cara;
+        this.info = 'Cara!';
+      }else{
+        this.moeda = this.coroa;
+        this.info = 'Coroa!'
+      }
+    }, 1500);
   }
 }
