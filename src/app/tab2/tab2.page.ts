ab5541d2466a9108479975e3632ee879017d1982
Gabriel Pozato <gabriel.pozato@etec.sp.gov.br>
2023-03-03 21:10:12 -0300

    finalizando programação de soma dos tentos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,10 +5,66 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page{
+  somarTento = '';
+  resultado1= 0;
+  resultado2= 0;
+  pontos1 = 0;
+  pontos2 = 0;
+  valendo = 1;
 
   fundo = 'assets/truco.jpg';
 
   constructor() {}
+
+  normal(){
+    this.valendo=1;
+  }
+
+  truco(){
+    this.valendo=3;
+  }
+
+  facao(){
+    this.valendo=6;
+  }
+
+  marreco(){
+    this.valendo=9;
+  }
+
+  ladrao(){
+    this.valendo=12;
+  }
+
+  somaEsq(){
+    this.pontos1 = this.valendo + this.pontos1;
+  }
+
+  somaDir(){
+    this.pontos2 = this.valendo + this.pontos2;
+  }
+
+  menosEsq(){
+    this.pontos1 = this.valendo - this.pontos1;
+  }
+
+  menosDir(){
+    this.pontos2 = this.valendo - this.pontos2;
+  }
+
+  resultado(){
+    if (this.pontos1){
+
+    }
+  }
+
+
+  limparJogo() {
+    this.resultado1 = 0;
+    this.resultado2 = 0;
+    this.pontos1 = 0;
+    this.pontos2 = 0;
+  }
 
 }
