d710524790985fb45fb8fe13bbda5911bf54e204
Gabriel Pozato <gabriel.pozato@etec.sp.gov.br>
2023-03-17 21:02:54 -0300

    finalização projeto

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  tamanho = 6;
+  valorAtual = 0;
+
+  imgDado = ['assets/dice-red.png', 'assets/dice.gif'];
+  imagemDado = this.imgDado[0];
+
   constructor() {}
 
+  jogarDado() {
+    this.valorAtual = 0;
+    this.imagemDado = this.imgDado[1];
+      setTimeout(() => {
+        this.valorAtual = Math.floor(Math.random() * this.tamanho)+1;
+      },1000);
+    }
 }
